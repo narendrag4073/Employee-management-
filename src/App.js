f591a497a5a3251f4563a2cf7d3cf6d50c6e6094
narendra <narendrag4073@gmail.com>
2018-07-13 10:18:38 +0530

    app changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import './main.css';
-import MovieList from './containers/movie-list';
-import MovieDetail from './containers/movie-detail';
 import MemberList from './containers/MemberList';
 import AddMember from './containers/AddMember';
 import EditMember from './containers/EditMember';
+
 export default class App extends Component {
   render() {
     return (
